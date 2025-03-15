90e40caf82fa9ea47ecb4ffa1d5055bde9d6cd62
Arundhati Wandhekar <134205007+ArundhatiWandhekar25@users.noreply.github.com>
2025-03-15 18:30:38 +0530

    Add files via upload
     how to create Different components and react elements and about Jsx

--- Namaste_React/app.js
+++ Namaste_React/app.js
@@ -1,10 +1,47 @@
-const heading1 = React.createElement("h1",{
-    id:"head1"
-},"Heading1");
-const heading2 = React.createElement("h2",{
-    id:"head2"
-},"Heading2");
-const container = React.createElement("div",{id:"container"},[heading1,heading2]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+//React Elements
+
+const heading = React.createElement("h1",{id:"heading1",class:"heading1"},"Namaste React");
+console.log(heading);
+const jsxHeading=<h1 id="heading" class="heading">Hello this is Arundhati💕</h1>
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+
+//React functional component
+const Title = ()=> (
+        <h1>
+            Namaste React
+        </h1>
+);
+const spanElement = <span>React span element</span>
+const NewComponent = ()=>{
+    <div>
+        <h1>
+            This is Arundhati💕
+        </h1>
+    </div>
+}
+const HeadingComponent = ()=>(
+    <div>
+       <h1> {100+400}</h1>
+        {/* {title} */}
+        <Title /> 
+        <h1>Namaste React functional component</h1>
+    </div>
+);
+
+const title = (
+    <h1>
+        {spanElement}
+        <HeadingComponent />
+        React Element
+    </h1>
+)
+
+
+const HeadingComponent1 = ()=><h1>This is React functional component</h1>;
+
+
+root.render(title);
